237494f0eabf92fa1185f0f4c10a8aee69d86984
cevr <seeve.c@gmail.com>
2025-09-28 13:30:19 -0400

    more refactor

--- core/model.ts
+++ core/model.ts
@@ -15,149 +15,149 @@ export enum Provider {
   Kimi = 'kimi',
 }
 
-const extractModel = Effect.fn('extractModel')(function* (
-  modelOption: Option.Option<string>,
-) {
-  const google = yield* Schema.Config(
-    'GEMINI_API_KEY',
-    Schema.NonEmptyString,
-  ).pipe(
-    Effect.option,
-    Effect.map((googleKey) =>
-      googleKey.pipe(
-        Option.map((googleKey) => {
-          const modelProvider = createGoogleGenerativeAI({
-            apiKey: googleKey,
-          });
-          return {
-            models: {
-              high: modelProvider('gemini-2.5-pro'),
-              low: modelProvider('gemini-2.0-flash-exp'),
-            },
-            provider: Provider.Gemini,
-          };
-        }),
+const extractModel = Effect.fn('extractModel')(
+  function* (modelOption: Option.Option<string>) {
+    const google = yield* Schema.Config(
+      'GEMINI_API_KEY',
+      Schema.NonEmptyString,
+    ).pipe(
+      Effect.option,
+      Effect.map((googleKey) =>
+        googleKey.pipe(
+          Option.map((googleKey) => {
+            const modelProvider = createGoogleGenerativeAI({
+              apiKey: googleKey,
+            });
+            return {
+              models: {
+                high: modelProvider('gemini-2.5-pro'),
+                low: modelProvider('gemini-2.0-flash-exp'),
+              },
+              provider: Provider.Gemini,
+            };
+          }),
+        ),
       ),
-    ),
-  );
-  const openai = yield* Schema.Config(
-    'OPENAI_API_KEY',
-    Schema.NonEmptyString,
-  ).pipe(
-    Effect.option,
-    Effect.map((openaiKey) =>
-      openaiKey.pipe(
-        Option.map((openaiKey) => {
-          return {
-            models: {
-              high: createOpenAI({ apiKey: openaiKey })('gpt-5'),
-              low: createOpenAI({ apiKey: openaiKey })('gpt-5-nano'),
-            },
-            provider: Provider.OpenAI,
-          };
-        }),
+    );
+    const openai = yield* Schema.Config(
+      'OPENAI_API_KEY',
+      Schema.NonEmptyString,
+    ).pipe(
+      Effect.option,
+      Effect.map((openaiKey) =>
+        openaiKey.pipe(
+          Option.map((openaiKey) => {
+            return {
+              models: {
+                high: createOpenAI({ apiKey: openaiKey })('gpt-5'),
+                low: createOpenAI({ apiKey: openaiKey })('gpt-5-nano'),
+              },
+              provider: Provider.OpenAI,
+            };
+          }),
+        ),
       ),
-    ),
-  );
-  const anthropic = yield* Schema.Config(
-    'ANTHROPIC_API_KEY',
-    Schema.NonEmptyString,
-  ).pipe(
-    Effect.option,
-    Effect.map((anthropicKey) =>
-      anthropicKey.pipe(
-        Option.map((anthropicKey) => {
-          return {
-            models: {
-              high: createAnthropic({ apiKey: anthropicKey })(
-                'claude-4-sonnet-20250514',
-              ),
-              low: createAnthropic({ apiKey: anthropicKey })(
-                'claude-3-5-haiku-20241022',
-              ),
-            },
-            provider: Provider.Anthropic,
-          };
-        }),
+    );
+    const anthropic = yield* Schema.Config(
+      'ANTHROPIC_API_KEY',
+      Schema.NonEmptyString,
+    ).pipe(
+      Effect.option,
+      Effect.map((anthropicKey) =>
+        anthropicKey.pipe(
+          Option.map((anthropicKey) => {
+            return {
+              models: {
+                high: createAnthropic({ apiKey: anthropicKey })(
+                  'claude-4-sonnet-20250514',
+                ),
+                low: createAnthropic({ apiKey: anthropicKey })(
+                  'claude-3-5-haiku-20241022',
+                ),
+              },
+              provider: Provider.Anthropic,
+            };
+          }),
+        ),
       ),
-    ),
-  );
-  const kimi = yield* Schema.Config('GROQ_API_KEY', Schema.NonEmptyString).pipe(
-    Effect.option,
-    Effect.map((groqKey) =>
-      groqKey.pipe(
-        Option.map((groqKey) => {
-          const modelProvider = createGroq({ apiKey: groqKey });
-          return {
-            models: {
-              high: modelProvider('moonshotai/kimi-k2-instruct'),
-              low: modelProvider('moonshotai/kimi-k2-instruct'),
-            },
-            provider: Provider.Kimi,
-          };
-        }),
+    );
+    const kimi = yield* Schema.Config(
+      'GROQ_API_KEY',
+      Schema.NonEmptyString,
+    ).pipe(
+      Effect.option,
+      Effect.map((groqKey) =>
+        groqKey.pipe(
+          Option.map((groqKey) => {
+            const modelProvider = createGroq({ apiKey: groqKey });
+            return {
+              models: {
+                high: modelProvider('moonshotai/kimi-k2-instruct'),
+                low: modelProvider('moonshotai/kimi-k2-instruct'),
+              },
+              provider: Provider.Kimi,
+            };
+          }),
+        ),
       ),
-    ),
-  );
-  const models = Option.reduceCompact(
-    [
-      google,
-      openai,
-      anthropic,
-      kimi as unknown as Option.Option<{
+    );
+    const models = Option.reduceCompact(
+      [
+        google,
+        openai,
+        anthropic,
+        kimi as unknown as Option.Option<{
+          models: { high: LanguageModel; low: LanguageModel };
+          provider: Provider;
+        }>,
+      ],
+      [] as {
         models: { high: LanguageModel; low: LanguageModel };
         provider: Provider;
-      }>,
-    ],
-    [] as {
-      models: { high: LanguageModel; low: LanguageModel };
-      provider: Provider;
-    }[],
-    (acc, model) => [...acc, model],
-  );
+      }[],
+      (acc, model) => [...acc, model],
+    );
 
-  if (models.length === 0) {
-    return yield* Effect.dieMessage('No model provider found');
-  }
+    if (models.length === 0) {
+      return yield* Effect.dieMessage('No model provider found');
+    }
 
-  const model = modelOption.pipe(
-    Option.flatMap((model) => matchEnum(Provider, model)),
-    Option.flatMap((model) =>
-      Option.fromNullable(models.find((m) => m.provider === model)?.models),
-    ),
-  );
-
-  return yield* Option.match(model, {
-    onNone: () =>
-      select(
-        'Select a model',
-        models.map((model) => ({
-          value: model.models,
-          label: Match.value(model.provider).pipe(
-            Match.when(Provider.Gemini, () => 'Gemini'),
-            Match.when(Provider.OpenAI, () => 'OpenAI'),
-            Match.when(Provider.Anthropic, () => 'Anthropic'),
-            Match.when(Provider.Kimi, () => 'Kimi'),
-            Match.exhaustive,
-          ),
-        })),
+    const model = modelOption.pipe(
+      Option.flatMap((model) => matchEnum(Provider, model)),
+      Option.flatMap((model) =>
+        Option.fromNullable(models.find((m) => m.provider === model)?.models),
       ),
-    onSome: Effect.succeed,
-  });
-});
+    );
+
+    return yield* Option.match(model, {
+      onNone: () =>
+        select(
+          'Select a model',
+          models.map((model) => ({
+            value: model.models,
+            label: Match.value(model.provider).pipe(
+              Match.when(Provider.Gemini, () => 'Gemini'),
+              Match.when(Provider.OpenAI, () => 'OpenAI'),
+              Match.when(Provider.Anthropic, () => 'Anthropic'),
+              Match.when(Provider.Kimi, () => 'Kimi'),
+              Match.exhaustive,
+            ),
+          })),
+        ),
+      onSome: Effect.succeed,
+    });
+  },
+  Effect.mapError(() =>
+    ValidationError.invalidArgument({
+      _tag: 'Empty',
+    }),
+  ),
+);
 
 export const model = Options.text('model').pipe(
   Options.withAlias('m'),
   Options.optional,
-  Options.mapEffect((x) =>
-    extractModel(x).pipe(
-      Effect.mapError((e) =>
-        ValidationError.invalidArgument({
-          _tag: 'Empty',
-        }),
-      ),
-    ),
-  ),
+  Options.mapEffect(extractModel),
 );
 export class Model extends Context.Tag('Model')<
   Model,
