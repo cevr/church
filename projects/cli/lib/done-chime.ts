208f1412b83393c96fa088732ebb2ddec432f8f8
cevr <seeve.c@gmail.com>
2025-11-12 20:00:15 -0500

    add new chime

--- projects/cli/lib/done-chime.ts
+++ projects/cli/lib/done-chime.ts
@@ -1,4 +1,14 @@
 import { Effect } from 'effect';
+import { $, } from 'bun';
+import { Path } from '@effect/platform';
+
+
 
 // this is playing on the terminal
-export const doneChime = Effect.sync(() => process.stdout.write('\x07'));
+export const doneChime = Effect.gen(function* () {
+const path = yield* Path.Path;
+const assetPath = path.join(process.cwd(), 'assets', 'notification.mp3');
+
+
+yield* Effect.tryPromise(async () => await $`afplay ${assetPath} -v 0.15`);
+})
